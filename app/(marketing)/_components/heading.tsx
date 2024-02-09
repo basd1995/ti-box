8a58eafd6a14e82c9e76233140c935ed2f089db1
basd1995 <1035102183@qq.com>
2024-02-09 23:42:47 +0800

    feat: create nextjs app and marketing page init

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Tickets, Documents, & Plans. Unified. Welcome to <span className="underline">TiBox</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        TiBox is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+    </div>
+  );
+}
+
+export default Heading;
